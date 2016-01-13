758b6f934fe5c7d4066ec29691877d982b5794b6
gcleenew <cleenewerckguillaume@gmail.com>
2016-01-13 15:55:38 +0100

    ajout de la main compiler et des tests sur persistent vector

---
+++ src/org/isep/matrixDSL/java/Compiler.java
@@ -0,0 +1,8 @@
+package org.isep.matrixDSL.java;
+import org.isep.matrixDSL.java.PersistentVectorCompiler;
+
+public class Compiler {
+	public static void main(String[] args) {
+		PersistentVectorCompiler.test();
+	}
+}
--- src/org/isep/matrixDSL/java/PersistentVectorCompiler.java
+++ src/org/isep/matrixDSL/java/PersistentVectorCompiler.java
@@ -1,11 +1,23 @@
 package org.isep.matrixDSL.java;
 
+import java.util.ArrayList;
+
 import clojure.lang.PersistentVector;
 
 public class PersistentVectorCompiler {
-	public static String test(PersistentVector vector) {
+	public static String test() {
 		
-		 return "TODO";
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		array.add(1);
+		array.add(3);
+		array.add(5);
+		PersistentVector test = PersistentVector.create(array);
+		System.out.print("Array ? : "+test.toArray()+"..");
+		int cnt = test.count();
+		System.out.print(cnt);
+		
+		System.out.println("object ? : "+test.tail.toString());
+		return "TODO";
 	}
 	
 	public static int hello(int n) {
