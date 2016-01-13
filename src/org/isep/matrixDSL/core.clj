576d76acc2b7b9f5ed8d99336f220289b09f9e6a
gcleenew <cleenewerckguillaume@gmail.com>
2016-01-13 18:04:28 +0100

    debug in compiler for add/sub

--- src/org/isep/matrixDSL/core.clj
+++ src/org/isep/matrixDSL/core.clj
@@ -6,13 +6,13 @@
    "S = addsub space (addsub)*
     <addsub> = add | sub | vector
     add = addsub space <'+'> space vector
-    sub = addsub <'-'> vector
+    sub = addsub space <'-'> space vector
     <space> = <#'\\s*'>
     vector = <'['> vsize <','> argument <']'>
     vsize = #'\\d+'
     argument= <'%'>#'[0-9]+'"))
-(vectorParser "[3,%1] + [3,%2]")
-(PersistentVectorCompiler/test (vectorParser "[3,%2] + [3,%2] + [3,%3] + [3,%4] + [3,%6]"))
+(vectorParser "[3,%2] + [3,%2] + [3,%3] + [3,%4] - [3,%6]")
+(PersistentVectorCompiler/test (vectorParser "[3,%2] + [3,%2] + [3,%3] + [3,%4] - [3,%10]"))
 
 (comment
 	(def vector-arith
