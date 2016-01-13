92b6ca85529cbe11bb2954fc7a4e9650d028e0a5
julcity <julcity77@gmail.com>
2016-01-13 16:43:37 +0100

    exec a method from byte code

--- src/org/isep/matrixDSL/asm/ByteToClass.java
+++ src/org/isep/matrixDSL/asm/ByteToClass.java
@@ -1,9 +1,38 @@
 package org.isep.matrixDSL.asm;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class ByteToClass extends ClassLoader {
 	
 	public void byteToClass(byte[] bytes) {
 		Class addClass = defineClass("Addition", bytes, 0, bytes.length);
+		
+		try {
+			String[] tab = {" "};
+			Method method = addClass.getMethod("add", int.class, int.class);
+			System.out.print(method.invoke(null,1, 2));
+			
+			
+			
+			
+			
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		resolveClass(addClass);
 	}
--- src/org/isep/matrixDSL/asm/TestAsm.java
+++ src/org/isep/matrixDSL/asm/TestAsm.java
@@ -7,14 +7,22 @@ import org.objectweb.asm.util.ASMifier;
 public class TestAsm {
 
 	public static void main(String[] args) {
-		 String[] tab =
-		 {"bin/org/isep/matrixDSL/asm/Addition.class"};
+//		 String[] tab =
+//		 {"bin/org/isep/matrixDSL/asm/Addition.class"};
+//		 try {
+//		 ASMifier.main(tab);
+//		 } catch (Exception e) {
+//		 // TODO Auto-generated catch block
+//		 e.printStackTrace();
+//		 }
+		 
+		 ByteToClass byteToClass = new ByteToClass();
 		 try {
-		 ASMifier.main(tab);
-		 } catch (Exception e) {
-		 // TODO Auto-generated catch block
-		 e.printStackTrace();
-		 }
+			byteToClass.byteToClass(AdditionDump.dump());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
